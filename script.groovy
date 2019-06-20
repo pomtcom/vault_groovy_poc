6f29f00ff20ac1cdcb5b7ee0732666d4940a38e2
Tawatchai Phetdumrongsakul <tawatchaiphetdumrongsakul@tdg-m0031.local>
2019-06-20 14:59:25 +0700

    checkpoint for create secret_id

--- script.groovy
+++ script.groovy
@@ -11,38 +11,25 @@ node {
 
     stage('create secret_id'){
         print 'creating secret_id'
-
         // POST
-    def post = new URL("http://10.198.105.221:8200/v1/auth/approle/role/vault_poc_role/secret-id").openConnection();
-    def message = '{"message":"this is a message"}'
-    post.setRequestMethod("POST")
-    post.setDoOutput(true)
-    post.setRequestProperty("X-Vault-Token", "s.3aVA6ckaOumc6N7WHTZHZ34a")
-    post.getOutputStream().write(message.getBytes("UTF-8"));
-    // println(postRC);
-    if(post.getResponseCode().equals(200)) {
-        def jsonResponse = post.getInputStream().getText() ;
-        // JSONObject responseJson = JSON.parse(post.getInputStream().getText());
-        // def secret_id = responseJson['data']['secret_id'];
-        print('before JsonSlurper');
-        def slurped = new JsonSlurper().parseText(jsonResponse);
-        print('after JsonSlurper');
+        def post = new URL("http://10.198.105.221:8200/v1/auth/approle/role/vault_poc_role/secret-id").openConnection();
+        def message = '{"message":"this is a message"}'
+        post.setRequestMethod("POST")
+        post.setDoOutput(true)
+        post.setRequestProperty("X-Vault-Token", "s.3aVA6ckaOumc6N7WHTZHZ34a")
+        post.getOutputStream().write(message.getBytes("UTF-8"));
+        // println(postRC);
+        if(post.getResponseCode().equals(200)) {
+            def jsonResponse = post.getInputStream().getText() ;
+            def jsonSlurped = new JsonSlurper().parseText(jsonResponse);
+            
+            // to add try catch for accessing json
+            def secret_id = jsonSlurped['data']['secret_id'];
+            print('secret_id is ' + secret_id)
 
-        print(slurped.data);
-        print('after print .data');
-
-        print(slurped.data['secret_id']);
-        print('after print secret_id');
-
-        print(slurped['data']['secret_id']);
-        print('after access like nodejs');
-
-        // print('secret_id is ' + secret_id['data']);
-    }else{
-        println('http error response code ' + post.getResponseCode());
-    }
-
-
+        }else{
+            println('http error response code ' + post.getResponseCode());
+        }
     }
 
     stage('generate role_token'){

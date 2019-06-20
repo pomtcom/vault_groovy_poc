f36a21fc79089b46cc720c3a7872f80bfb61ba6e
Tawatchai Phetdumrongsakul <tawatchaiphetdumrongsakul@tdg-m0031.local>
2019-06-20 15:24:42 +0700

    test post body

--- script.groovy
+++ script.groovy
@@ -1,19 +1,20 @@
 import groovy.json.JsonSlurper
 
 print 'vault groovy poc is starting'
-
+def role_id = params.role_id
 
 node {
 
     stage('get role_id') {
         print 'getting role_id'
+        print 'role id is ' + role_id
     }
 
     stage('create secret_id'){
         print 'creating secret_id'
         // POST
         def post = new URL("http://10.198.105.221:8200/v1/auth/approle/role/vault_poc_role/secret-id").openConnection();
-        def message = '{"message":"this is a message"}'
+        def message = '{}'
         post.setRequestMethod("POST")
         post.setDoOutput(true)
         post.setRequestProperty("X-Vault-Token", "s.3aVA6ckaOumc6N7WHTZHZ34a")
@@ -34,6 +35,11 @@ node {
 
     stage('generate role_token'){
         print 'generating role_token'
+        def post = new URL("http://10.198.105.221:8200/v1/auth/approle/login").openConnection();
+        def message = '{"role_id": "' + role_id + '",' + '"secret_id": "' + secret_id + '"}';
+        post.setRequestMethod("POST");
+        post.setDoOutput(true);
+        print('message to send is ' + message);
     }
 
     

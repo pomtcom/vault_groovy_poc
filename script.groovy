93fb409c34951729c3e07db2d3b04a92503e111e
Tawatchai Phetdumrongsakul <tawatchaiphetdumrongsakul@tdg-m0031.local>
2019-06-20 10:14:06 +0700

    adding stage

--- script.groovy
+++ script.groovy
@@ -1,1 +1,19 @@
 print 'vault groovy poc is starting'
+
+
+node {
+
+    stage('get role_id') {
+        print 'getting role_id'
+    }
+
+    stage('create secret_id'){
+        print 'creating secret_id'
+    }
+
+    stage('generate role_token'){
+        print 'generating role_token'
+    }
+
+    
+}

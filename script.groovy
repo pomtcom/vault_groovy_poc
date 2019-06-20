8ebe5c3b1e5dcdf149ee1ea117bdc397eece217b
Tawatchai Phetdumrongsakul <tawatchaiphetdumrongsakul@tdg-m0031.local>
2019-06-20 14:08:39 +0700

    test http

--- script.groovy
+++ script.groovy
@@ -9,6 +9,21 @@ node {
 
     stage('create secret_id'){
         print 'creating secret_id'
+
+        // POST
+    def post = new URL("http://10.198.105.221:8200/v1/auth/approle/role/vault_poc_role/secret-id").openConnection();
+    def message = '{"message":"this is a message"}'
+    post.setRequestMethod("POST")
+    post.setDoOutput(true)
+    post.setRequestProperty("X-Vault-Token", "s.3aVA6ckaOumc6N7WHTZHZ34a")
+    post.getOutputStream().write(message.getBytes("UTF-8"));
+    def postRC = post.getResponseCode();
+    println(postRC);
+    if(postRC.equals(200)) {
+        println(post.getInputStream().getText());
+    }
+
+
     }
 
     stage('generate role_token'){
